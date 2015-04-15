db0736b17f9abe5bb2bae55de40e9e1b7fc9b28c
G.Tjebbes <g.t@majerti.fr>
2015-04-15 17:58:34 +0200

    Update the version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='deform_extensions',
-    version='0.1',
+    version='0.2a',
     packages=['deform_extensions'],
     include_package_data=True,
     license='GPLv3',
